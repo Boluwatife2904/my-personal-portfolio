7830d8373e54a769ad048f1ad26a6748c3a82bce
Boluwatife2904 <sanvicola2000@gmail.com>
2020-11-25 02:58:51 +0100

    Linked Contact form to Netlify

--- js/custom.js
+++ js/custom.js
@@ -2,27 +2,8 @@
 
 	"use strict";
 
-	/* ----------------------------------------------------------- */
-	/*  FUNCTION TO STOP LOCAL AND YOUTUBE VIDEOS IN SLIDESHOW
-    /* ----------------------------------------------------------- */
-
-	function stop_videos() {
-		var video = document.getElementById("video");
-		if (video.paused !== true && video.ended !== true) {
-			video.pause();
-		}
-		$('.youtube-video')[0].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
-	}
 
 	$(document).ready(function() {
-
-		/* ----------------------------------------------------------- */
-		/*  STOP VIDEOS
-        /* ----------------------------------------------------------- */
-
-		$('.slideshow nav span').on('click', function () {
-			stop_videos();
-		});
 
 		/* ----------------------------------------------------------- */
 		/*  FIX REVEALATOR ISSUE AFTER PAGE LOADED
@@ -95,33 +76,33 @@
 		/*  AJAX CONTACT FORM
         /* ----------------------------------------------------------- */
 
-		$(".contactform").on("submit", function() {
-			$(".output_message").text("Sending...");
+	// 	$(".contactform").on("submit", function() {
+	// 		$(".output_message").text("Sending...");
 
-			var form = $(this);
-			$.ajax({
-				url: form.attr("action"),
-				method: form.attr("method"),
-				data: form.serialize(),
-				success: function(result) {
-					if (result == "success") {
-						$(".form-inputs").css("display", "none");
-						$(".box p").css("display", "none");
-						$(".contactform").find(".output_message").addClass("success");
-						$(".output_message").text("Message Sent!");
-					} else {
-						$(".tabs-container").css("height", "440px");
+	// 		var form = $(this);
+	// 		$.ajax({
+	// 			url: form.attr("action"),
+	// 			method: form.attr("method"),
+	// 			data: form.serialize(),
+	// 			success: function(result) {
+	// 				if (result == "success") {
+	// 					$(".form-inputs").css("display", "none");
+	// 					$(".box p").css("display", "none");
+	// 					$(".contactform").find(".output_message").addClass("success");
+	// 					$(".output_message").text("Message Sent!");
+	// 				} else {
+	// 					$(".tabs-container").css("height", "440px");
 
-						$(".contactform").find(".output_message").addClass("error");
-						$(".output_message").text("Error Sending!");
-					}
-				}
-			});
+	// 					$(".contactform").find(".output_message").addClass("error");
+	// 					$(".output_message").text("Error Sending!");
+	// 				}
+	// 			}
+	// 		});
 
-			return false;
-		});
+	// 		return false;
+	// 	});
 
-	});
+	// });
 
 	$(document).keyup(function(e) {
 
